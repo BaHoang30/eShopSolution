b969489b7a30fabcff954d31e73c17b09c917c78
Hoang Nguyen <nguyenbahoang321@gmail.com>
2020-09-15 20:53:55 +0700

    Revert "add swagger"
    This reverts commit c6ab10b8f2fb29ba76d82ba81b699ec3e953df40.

--- eShopSolution.Aplication/Catalog/Products/PublicProductService.cs
+++ eShopSolution.Aplication/Catalog/Products/PublicProductService.cs
@@ -9,7 +9,7 @@ using System;
 
 namespace eShopSolution.Aplication.Catalog.Products
 {
-    public class PublicProductService : IPublicProductService
+    class PublicProductService : IPublicProductService
     {
         private readonly EShopDbContext _context;
         public PublicProductService(EShopDbContext context)
@@ -17,31 +17,9 @@ namespace eShopSolution.Aplication.Catalog.Products
             _context = context;
         }
 
-        public async Task<List<ProductViewModel>> GetAll()
+        public Task<List<ProductViewModel>> GetAll()
         {
-            var query = from p in _context.Products
-                        join pt in _context.ProductTranslations on p.Id equals pt.ProductId
-                        join pic in _context.ProductInCategories on p.Id equals pic.ProductId
-                        join c in _context.Categories on pic.CategoryId equals c.Id
-                        select new { p, pt, pic };
-
-            var data = await query.Select(x => new ProductViewModel()
-            {
-                Id = x.p.Id,
-                Name = x.pt.Name,
-                DateCreated = x.p.DateCreated,
-                Description = x.pt.Description,
-                Details = x.pt.Details,
-                LanguageId = x.pt.LanguageId,
-                OriginalPrice = x.p.OriginalPrice,
-                Price = x.p.Price,
-                SeoAlias = x.pt.SeoAlias,
-                SeoDescription = x.pt.SeoDescription,
-                SeoTitle = x.pt.SeoTitle,
-                Stock = x.p.Stock,
-                ViewCount = x.p.ViewCount
-            }).ToListAsync();
-            return data;
+            throw new NotImplementedException();
         }
 
         public async Task<PagedResult<ProductViewModel>> GetAllByCategoryId(GetPublicProductPagingRequest request)
@@ -88,5 +66,9 @@ namespace eShopSolution.Aplication.Catalog.Products
             return pagedResult;
         }
 
+        Task<List<ProductViewModel>> IPublicProductService.GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
