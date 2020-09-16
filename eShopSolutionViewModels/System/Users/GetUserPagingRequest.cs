f2f04672cd7a107da8053f8dc2338e71f6518598
Hoang Nguyen <nguyenbahoang321@gmail.com>
2020-09-16 18:21:17 +0700

    users api

---
+++ eShopSolutionViewModels/System/Users/GetUserPagingRequest.cs
@@ -0,0 +1,12 @@
+﻿using eShopSolutionViewModels.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolutionViewModels.System.Users
+{
+    public class GetUserPagingRequest : PagingRequestBase
+    {
+        public string Keyword { get; set; }
+    }
+}
---
+++ eShopSolutionViewModels/System/Users/RoleAssignRequest.cs
@@ -0,0 +1,13 @@
+﻿using eShopSolutionViewModels.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolutionViewModels.System.Users
+{
+    public class RoleAssignRequest
+    {
+        public Guid Id { get; set; }
+        public List<SelectItem> Roles { get; set; } = new List<SelectItem>();
+    }
+}
